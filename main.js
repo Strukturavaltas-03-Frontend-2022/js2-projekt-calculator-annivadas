4264b0aad0f4f801ef917918204eacbcbfd95b39
annivadas <anni.vadas@gmail.com>
2022-08-26 17:44:29 +0200

    nagyon nehéz

---
+++ main.js
@@ -0,0 +1,19 @@
+const one = document.querySelector('.one');
+const two = document.querySelector('two');
+const three = document.querySelector('tree');
+const four = document.querySelector('four');
+const five = document.querySelector('five');
+const six = document.querySelector('six');
+const seven = document.querySelector('seven');
+const eight = document.querySelector('eght');
+const nine = document.querySelector('nine');
+const nulla = document.querySelector('nulla');
+
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const multiplay = document.querySelector('.multiplay');
+const divide = document.querySelector('.divide');
+
+const equal = document.querySelector('.equal');
+
+let currentInput = ' ';
